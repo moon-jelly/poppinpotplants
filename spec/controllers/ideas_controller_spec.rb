857d695d2acf09512a33194f02f39ec0a361dc24
Jevon Wright <jevon@jevon.org>
2017-10-27 11:57:54 +1300

    Add rpec for GET /ideas/show/id

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,10 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe IdeasController, type: :controller do
+  render_views # so that templates are rendered
+
   describe 'GET #index' do
     it "renders the index template" do
       get :index
       expect(response).to render_template("index")
     end
-  end 
+
+    it "can show an idea" do
+      idea = Idea.create!(name: "Drink soy milk", description: "It is full of beans!")
+      get :show, params: { id: idea.id }
+      expect(response).to render_template("show")
+      expect(response.body).to include(idea.name)
+      expect(response.body).to include(idea.description)
+    end
+  end
 end
